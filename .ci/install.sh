aadb1707150eb668a881cd02e29e47038f582008
stnolting <stnolting@gmail.com>
2020-07-06 00:20:06 +0200

    neorv32 image generator is compiled outside of container

--- .ci/install.sh
+++ .ci/install.sh
@@ -3,29 +3,26 @@
 # Abort if any command returns != 0
 set -e
 
-# Download toolchain
-echo ""
-echo "Downloading prebuilt RISC-V GCC toolchain..."
-echo ""
-rm -f riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz
-wget https://github.com/stnolting/riscv_gcc_prebuilt/raw/master/data/riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz
-
-# Decompress toolchain
-rm -rf riscv
-mkdir riscv
-tar -xzf riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz -C riscv/
-pwd
-rm -f riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz
-
-# Test toolchain
-riscv32-unknown-elf-gcc -v
-
-# Download RISC-V compliance test suite
-echo ""
-echo "Downloading RISC-V Compliance Test Suite"
-echo ""
-rm -rf riscv-compliance
-git clone https://github.com/riscv/riscv-compliance.git
+## Download toolchain
+#echo ""
+#echo "Downloading prebuilt RISC-V GCC toolchain..."
+#echo ""
+#rm -f riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz
+#wget https://github.com/stnolting/riscv_gcc_prebuilt/raw/master/data/riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz
+#
+## Decompress toolchain
+#rm -rf riscv
+#mkdir riscv
+#tar -xzf riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz -C riscv/
+#pwd
+#rm -f riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz
+#
+## Download RISC-V compliance test suite
+#echo ""
+#echo "Downloading RISC-V Compliance Test Suite"
+#echo ""
+#rm -rf riscv-compliance
+#git clone https://github.com/riscv/riscv-compliance.git
 
 # Download NEORV32 project
 echo ""
@@ -33,6 +30,8 @@ echo "Downloading NEORV32 Project..."
 echo ""
 rm -rf neorv32
 git clone https://github.com/stnolting/neorv32.git
+# check toolchain and compile image generator
+make -C neorv32/sw/example/blink_led check
 
 # Copy neorv32 target folder into test suite
 echo ""
