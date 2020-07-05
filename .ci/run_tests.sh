e840c1f50e2749ed9f806eb1bfb4464c10061367
stnolting <stnolting@gmail.com>
2020-07-05 23:22:55 +0200

    bug fixes...

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -10,7 +10,9 @@ homedir=$homedir/..
 # neorv32 home folder
 NEORV32_HOME_DIR=$homedir/neorv32
 
-echo $NEORV32_HOME_DIR
+echo ""
+echo "Starting compliance tests..."
+echo ""
 
 # Run tests
 make -C riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32i     RISCV_ISA=rv32i     RISCV_PREFIX=riscv32-unknown-elf-
