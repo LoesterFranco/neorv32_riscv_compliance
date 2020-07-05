a18086df55027122e4af6e6d048464c05ddf2ab8
stnolting <stnolting@gmail.com>
2020-07-05 23:44:26 +0200

    bug fixes...

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -15,7 +15,7 @@ echo "Starting compliance tests..."
 echo ""
 
 # Run tests
-make -C riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32i     RISCV_ISA=rv32i     RISCV_PREFIX=riscv32-unknown-elf-
-make -C riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32im    RISCV_ISA=rv32im    RISCV_PREFIX=riscv32-unknown-elf-
-make -C riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32imc   RISCV_ISA=rv32imc   RISCV_PREFIX=riscv32-unknown-elf-
-make -C riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32Zicsr RISCV_ISA=rv32Zicsr RISCV_PREFIX=riscv32-unknown-elf-
+make -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32i     RISCV_ISA=rv32i     RISCV_PREFIX=riscv32-unknown-elf-
+make -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32im    RISCV_ISA=rv32im    RISCV_PREFIX=riscv32-unknown-elf-
+make -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32imc   RISCV_ISA=rv32imc   RISCV_PREFIX=riscv32-unknown-elf-
+make -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32Zicsr RISCV_ISA=rv32Zicsr RISCV_PREFIX=riscv32-unknown-elf-
