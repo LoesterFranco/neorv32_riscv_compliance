b699d1524a4c152991c028df5fb85e9e1426f9ab
stnolting <stnolting@gmail.com>
2020-07-05 22:25:33 +0200

    minor edits

--- .ci/install.sh
+++ .ci/install.sh
@@ -10,6 +10,7 @@ echo ""
 wget https://github.com/stnolting/riscv_gcc_prebuilt/raw/master/data/riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz
 
 # Decompress
+rm -rf riscv
 mkdir riscv
 tar -xzf riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32.tar.gz -C riscv/
 pwd
@@ -21,12 +22,14 @@ ls -al riscv/
 echo ""
 echo "Downloading RISC-V Compliance Test Suite"
 echo ""
+rm -rf riscv-compliance
 git clone https://github.com/riscv/riscv-compliance.git
 
 # Download NEORV32 project
 echo ""
 echo "Downloading NEORV32 Project..."
 echo ""
+rm -rf neorv32
 git clone https://github.com/stnolting/neorv32.git
 
 # Copy neorv32 target folder into test suite
