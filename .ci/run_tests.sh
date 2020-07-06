02100883bd47ea0cc7dc7ed25c25a4066d513ad2
stnolting <stnolting@gmail.com>
2020-07-06 21:56:12 +0200

    added Zifencei test suite support

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -15,7 +15,8 @@ echo "Starting compliance tests..."
 echo ""
 
 # Run tests
-make --silent -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32i     RISCV_ISA=rv32i     RISCV_PREFIX=riscv32-unknown-elf-
-make --silent -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32im    RISCV_ISA=rv32im    RISCV_PREFIX=riscv32-unknown-elf-
-make --silent -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32imc   RISCV_ISA=rv32imc   RISCV_PREFIX=riscv32-unknown-elf-
-make --silent -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32Zicsr RISCV_ISA=rv32Zicsr RISCV_PREFIX=riscv32-unknown-elf-
+make --silent -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32i        RISCV_ISA=rv32i        RISCV_PREFIX=riscv32-unknown-elf-
+make --silent -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32im       RISCV_ISA=rv32im       RISCV_PREFIX=riscv32-unknown-elf-
+make --silent -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32imc      RISCV_ISA=rv32imc      RISCV_PREFIX=riscv32-unknown-elf-
+make --silent -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32Zicsr    RISCV_ISA=rv32Zicsr    RISCV_PREFIX=riscv32-unknown-elf-
+make --silent -C $homedir/riscv-compliance NEORV32_HOME=$NEORV32_HOME_DIR RISCV_TARGET=neorv32 RISCV_DEVICE=rv32Zifencei RISCV_ISA=rv32Zifencei RISCV_PREFIX=riscv32-unknown-elf-
